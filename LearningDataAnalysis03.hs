4c0cda570d0daa878a76ed2f7525e8323291441d
nassim <nassim@goodsid.io>
2018-10-31 10:13:09 +0100

    add regex error detection

--- LearningDataAnalysis03.hs
+++ LearningDataAnalysis03.hs
@@ -3,10 +3,47 @@ module LearningDataAnalysis03 where
     import Data.Either
     import Text.CSV
     import Text.Regex.Posix ((=~))
-    
+    import LearningDataAnalysis01
 
     myGrep :: String -> String -> IO()
     myGrep myRegex filename = do
       fileSlurp <- readFile filename
       mapM_ putStrLn $
         filter (=~ myRegex) (lines fileSlurp)
+
+    identifyMatchingFields :: (String -> Bool) -> [String] -> [String] -> Integer -> [(String, String, String)]
+    identifyMatchingFields myStringCmpFunc record headings idColumnIndex = filter
+        (\(_,_,field) -> myStringCmpFunc field)
+      keyvalue
+      where
+        nfields = length headings
+        keyvalue = zip3 (replicate nfields (genericIndex record idColumnIndex)) headings record
+
+    identifyInCSV :: (String -> Bool) -> CSV -> String -> Either String [(String, String, String)]
+    identifyInCSV myFieldFunc csv idColumn = either
+        Left
+        (\ci -> Right $ concatMap
+          (\record ->
+            identifyMatchingFields
+            myFieldFunc record (head csv) ci
+          )
+          (tail csv)
+        )
+        columnIndex
+        where
+          headings = head csv
+          columnIndex = getColumnInCSV csv idColumn
+
+    identifyInCSVFile :: (String -> Bool) -> String -> String -> IO(Either String [(String, String, String)])
+    identifyInCSVFile myStringCmpFunc inFileName idColumn = do
+      records <- parseCSVFromFile inFileName
+      return $ either
+        (\err -> Left "This does not appear to be a CSV File")
+        (\csv -> identifyInCSV myStringCmpFunc (init csv) idColumn) records
+
+    identifyInCSVFileFromColumn :: (String -> Bool) -> String -> String -> String -> IO(Either String [(String, String, String)])
+    identifyInCSVFileFromColumn myRegexFunc inFileName idColumn desiredHeading = do
+      allFields <- identifyInCSVFile myRegexFunc inFileName idColumn
+      return $ either
+        Left
+        (\af -> Right $ filter (\(_, heading, _) -> heading == desiredHeading) af) allFields
--- Main.hs
+++ Main.hs
@@ -8,10 +8,11 @@ module Main (main) where
     import Database.HDBC.Sqlite3
     -- install: cabal install csv
     import Text.CSV
+    import Text.Regex.Posix ((=~))
 
     import System.Environment (getArgs)
 
-    -- application of function to a column
+    -- -- application of function to a column
     -- main = do
     --     max <- applyToColumnInCSVFile (maximum . readColumn) "all_week.csv" "mag"
     --     min <- applyToColumnInCSVFile (minimum . readColumn) "all_week.csv" "mag"
@@ -20,7 +21,7 @@ module Main (main) where
     --     print min
     --     print avg
 
-    -- application of CSV to SQLite conversion
+    -- -- application of CSV to SQLite conversion
     -- main = convertCSVFileToSql "all_week.csv" "earthquakes.sql" "oneWeek" [
     --     "time TEXT", "latitude REAL", "longitude REAL", 
     --     "depth REAL", "mag REAL", "magType REAL", "nst INTEGER", 
@@ -29,7 +30,7 @@ module Main (main) where
     --     "horizontalError REAL", "depthError REAL", "magError REAL", 
     --     "magNst INTEGER", "status TEXT", "locationSource TEXT", "magSource TEXT"]
 
-    -- SQLite querying application
+    -- -- SQLite querying application
     -- main = do
     --     conn <- connectSqlite3 "earthquakes.sql"
     --     magnitudes <- quickQuery' conn "SELECT mag FROM oneWeek" []
@@ -37,14 +38,31 @@ module Main (main) where
     --     let avg = average magnitudesDouble
     --     print avg
 
-    -- Incorrect data application
+    -- -- Incorrect data application
     -- main = do
     --   csv <- parseCSVFromFile "poorFieldCounts.csv"
     --   let count = either Left (\csv -> Right $ lineNumbersWithIncorrectCount csv) csv
     --   print count
 
-    -- Application of custom grep ./main Betty poorFieldCounts.csv
+    -- -- Application of custom grep ./main Betty poorFieldCounts.csv
+    -- main :: IO()
+    -- main = do
+    --   (myRegex:filenames) <- getArgs
+    --   mapM_ (\filename -> myGrep myRegex filename) filenames
+
+    -- -- Application of simple regex application to CSV
+    -- main :: IO()
+    -- main = do
+    --   -- Search for empty fields
+    --   tmp <- identifyInCSVFile (\x -> x =~ "^\\s*$") "poorData.csv" "Number"
+    --   print tmp
+
+    -- Application of invalid date search
     main :: IO()
     main = do
-      (myRegex:filenames) <- getArgs
-      mapM_ (\filename -> myGrep myRegex filename) filenames
+      tmp <- identifyInCSVFileFromColumn
+        (\x -> not (x =~ "^[1-9][0-9]?/[1-9][0-9]?/[12][0-9][0-9][0-9]$"))
+        "poorData.csv"
+        "Number"
+        "Birthday"
+      print tmp
